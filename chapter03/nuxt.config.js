98d6eed0fe1f012b02adcb1c44aba35bec7d610e
DanielGara <yo@danielgara.com>
2021-01-23 08:19:17 -0500

    imp: chapter 03 completed

---
+++ chapter03/nuxt.config.js
@@ -0,0 +1,46 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'assets/scss/main.scss',
+    'foundation-sites/dist/css/foundation.min.css',
+    'foundation-icon-fonts/foundation-icons.css',
+    'assets/less/main.less',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/client-only/foundation.client.js', // only in client side
+    '~/plugins/client-only/motion-ui.client.js',
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
